565f5f011d3c4ad4507189e631f121055d5f6ac7
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-14 11:14:18 +0100

    Render todos as list items

--- to-do-list-app/src/components/App/App.js
+++ to-do-list-app/src/components/App/App.js
@@ -1,22 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import List from '../List/List.js';
 import Input from '../Input/Input.js';
 
 function App() {
-	const [todos, setTodos] = useState([]); 
-	const [entry, setEntry] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [entry, setEntry] = useState('');
 
 	function handleEntryChange(event) {
-			setEntry(event.target.value);
+		setEntry(event.target.value);
 	}
-// 	function handleFormSubmit(event) {
-// 		;
-// }
-// onFormSubmit = {handleFormSubmit}
+
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		// add the new entry
+		setTodos([...todos, entry]);
+		// then reset the input field
+		setEntry('');
+	}
+
 	return (
 		<main>
-			<Input entry = {entry} onEntryChange = {handleEntryChange} /> 
+			<Input
+				entry={entry}
+				onEntryChange={handleEntryChange}
+				onFormSubmit={handleFormSubmit}
+			/>
+			<List todos={todos} />
 		</main>
 	);
 }
--- to-do-list-app/src/components/List/List.js
+++ to-do-list-app/src/components/List/List.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import './List.css';
 
-export default function List() {}
+export default function List(props) {
+	return (
+		<ul>
+			{props.todos.map((todo, index) => (
+				<li key={index}>{todo}</li>
+			))}
+		</ul>
+	);
+}
